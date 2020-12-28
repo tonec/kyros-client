e979fc96a5a3a2f9edc6a4395a237aee85f11e63
Tony Coop <tony@thewholeworldwindow.co.uk>
2020-12-28 12:41:19 +0000

    Move Users to features.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import App from 'App'
 import { NotFound, PrivateRoute } from 'components'
 // import Home from 'views/Home/Home'
 import Login from 'features/Auth/Login'
-import Users from 'components/Users/Users'
+import Users from 'features/Users/Users'
 import FormLibrary from 'features/Library/Form'
 
 export default [
