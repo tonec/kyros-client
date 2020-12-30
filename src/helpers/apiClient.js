ebe445fff74e89da04b069d1a85a12fd9ffeeb52
Tony Coop <tony@thewholeworldwindow.co.uk>
2020-12-30 22:51:15 +0000

    Include cookie in server requests.

--- src/helpers/apiClient.js
+++ src/helpers/apiClient.js
@@ -2,14 +2,24 @@
 import axios from 'axios'
 import config from '../../config'
 
-export default function axiosClient() {
+export default function axiosClient(req) {
   const instance = axios.create({
-    baseURL: __SERVER__
-      ? `http://${config.apiHost}:${config.apiPort}/api`
-      : `http://${config.apiHost}:${config.apiPort}/api`,
+    baseURL: `http://${config.apiHost}:${config.apiPort}/api`,
   })
 
   instance.defaults.withCredentials = true
+
+  instance.interceptors.request.use(
+    conf => {
+      if (__SERVER__) {
+        if (req.header('cookie')) {
+          conf.headers.Cookie = req.header('cookie')
+        }
+      }
+      return conf
+    },
+    error => Promise.reject(error),
+  )
 
   instance.interceptors.response.use(
     response => response.data,
