6ff0c9d6c9277871a4bb9d2235d7ee346d14f1d4
Tony Coop <tony@thewholeworldwindow.co.uk>
2020-12-30 23:41:20 +0000

    Display users.

--- src/features/Users/Users.js
+++ src/features/Users/Users.js
@@ -4,30 +4,33 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { provideHooks } from 'redial'
 import { fetchUsers } from 'redux/modules/user/actions'
+import { getVisibleUsers } from 'redux/modules/user/selectors'
+import { getFullName } from 'utils/entity'
 import { Main } from 'components'
 
 function Users({ users }) {
+  console.log('users', users)
   const renderUsers = () => {
-    // return users.map(user => <li key={user.id}>{user.name}</li>)
+    return users.map(user => <li key={user.id}>{getFullName(user)}</li>)
   }
 
   return (
     <Main title="Users">
       <h2>Users lists:</h2>
-      {/* <ul>{renderUsers()}</ul> */}
+      <ul>{renderUsers()}</ul>
     </Main>
   )
 }
 
 Users.propTypes = {
-  // users: PropTypes.array,
+  users: PropTypes.array,
 }
 
 Users.defaultProps = {
-  // users: null,
+  users: null,
 }
 
-const mapState = ({ user }) => ({ users: user })
+const mapState = state => ({ users: getVisibleUsers(state) })
 
 const hooks = {
   fetch: ({ store }) => store.dispatch(fetchUsers()),
