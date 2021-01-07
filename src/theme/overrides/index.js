cc6d993738f062ca9537d95ca91fdf3b74d7606f
Tony Coop <tony@thewholeworldwindow.co.uk>
2021-01-07 23:02:39 +0000

    Override card.

--- src/theme/overrides/index.js
+++ src/theme/overrides/index.js
@@ -1,5 +1,6 @@
 import merge from 'lodash/merge'
 import button from './button'
+import card from './card'
 import checkbox from './checkbox'
 import input from './input'
 import formControlLabel from './formControlLabel'
@@ -10,6 +11,7 @@ import tableRow from './tableRow'
 export default theme => {
   return merge(theme.overrides, {
     MuiButton: merge(theme.MuiButton, button),
+    MuiCard: merge(theme.MuiCard, card),
     MuiCheckbox: merge(theme.MuiCheckbox, checkbox),
     MuiOutlinedInput: merge(theme.MuiOutlinedInput, input),
     MuiFormControlLabel: merge(theme.MuiFormControlLabel, formControlLabel),
