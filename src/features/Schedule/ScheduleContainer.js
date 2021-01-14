c9f21fff4f1e941240b3b738c0e3cf853f2e5b4d
Tony Coop <tony@thewholeworldwindow.co.uk>
2021-01-14 20:59:09 +0000

    Get dates in container.

--- src/features/Schedule/ScheduleContainer.js
+++ src/features/Schedule/ScheduleContainer.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { getQuery } from 'redux/modules/app/selectors'
+import { getScheduleDays } from 'services/date'
+import ScheduleHeader from './ScheduleHeader'
 import Schedule from './Schedule'
 
-function ScheduleContainer() {
-  return <Schedule />
+function ScheduleContainer({ query }) {
+  const days = getScheduleDays(query.ts || 'week')
+
+  return (
+    <>
+      <ScheduleHeader />
+      <Schedule days={days} />
+    </>
+  )
 }
 
-export default ScheduleContainer
+ScheduleContainer.propTypes = {
+  query: PropTypes.object,
+}
+
+ScheduleContainer.defaultProps = {
+  query: null,
+}
+
+const mapState = createStructuredSelector({
+  query: getQuery,
+})
+
+export default connect(mapState)(ScheduleContainer)
