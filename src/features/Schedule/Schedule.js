f601fdd5dda4884a8555672d602077aa9c91a8bc
Tony Coop <tony@thewholeworldwindow.co.uk>
2021-01-14 20:58:40 +0000

    Render dates.

--- src/features/Schedule/Schedule.js
+++ src/features/Schedule/Schedule.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import ScheduleHeader from './ScheduleHeader'
+import PropTypes from 'prop-types'
 
-function Schedule() {
-  return <ScheduleHeader />
+function Schedule({ days }) {
+  return days.map(day => <p key={day.getTime()}>{day.toString()}</p>)
+}
+
+Schedule.propTypes = {
+  days: PropTypes.arrayOf(PropTypes.instanceOf(Date)),
+}
+
+Schedule.defaultProps = {
+  days: [],
 }
 
 export default Schedule
