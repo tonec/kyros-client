bebf7a056197b2384edb99725ca221909a87a8d4
Tony Coop <tony@thewholeworldwindow.co.uk>
2021-02-08 16:21:27 +0000

    Fix typo and generic arg.

--- src/utils/getInitialValues.ts
+++ src/utils/getInitialValues.ts
@@ -1,6 +1,6 @@
-import { Obj } from 'types'
-
-function getInitialValues<T>(values: T>): Partial<T> {
+function getInitialValues<T extends { createdAt: string; updatedAt: string; id: string }>(
+  values: T,
+): any {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { createdAt, updatedAt, id, ...rest } = values
   return rest
